25499ba0b8c708f36b51ca2b7d72e35fe200ed86
leixt <tian-lei-happy@163.com>
2025-06-07 11:06:52 +0800

    fix: add GetToken

--- pkg/sdk/qiniu/qiniu.go
+++ pkg/sdk/qiniu/qiniu.go
@@ -15,14 +15,6 @@ type QiniuClient struct {
 }
 
 func NewClient(cfg QiniuConfig) *QiniuClient {
-	mac := qbox.NewMac(cfg.AccessKey, cfg.SecretKey)
-	putPolicy := storage.PutPolicy{
-		Scope: cfg.Bucket,
-		// 1小时有效期
-		Expires: 3600,
-	}
-	token := putPolicy.UploadToken(mac)
-
 	cfgs := storage.Config{}
 	switch cfg.Region {
 	case "z0":
@@ -36,9 +28,26 @@ func NewClient(cfg QiniuConfig) *QiniuClient {
 	}
 
 	uploader := storage.NewFormUploader(&cfgs)
-	return &QiniuClient{Config: cfg, Uploader: uploader, Token: token}
+	client := &QiniuClient{
+		Config:   cfg,
+		Uploader: uploader,
+	}
+	client.Token = client.generateToken()
+	//return &QiniuClient{Config: cfg, Uploader: uploader, Token: token}
+	return client
 }
 
+// 内部使用生成 Token 方法
+func (qc *QiniuClient) generateToken() string {
+	mac := qbox.NewMac(qc.Config.AccessKey, qc.Config.SecretKey)
+	putPolicy := storage.PutPolicy{
+		Scope:   qc.Config.Bucket,
+		Expires: 3600, // 单位：秒，1小时
+	}
+	return putPolicy.UploadToken(mac)
+}
+
+// UploadFile 表单上传
 func (qc *QiniuClient) UploadFile(file multipart.File, filename string) (string, error) {
 	ret := storage.PutRet{}
 	putExtra := storage.PutExtra{}
@@ -48,4 +57,9 @@ func (qc *QiniuClient) UploadFile(file multipart.File, filename string) (string,
 		return "", err
 	}
 	return fmt.Sprintf("%s/%s", qc.Config.Domain, ret.Key), nil
+}
+
+// GetToken 公共的 GetToken 方法，供前端获取上传凭证
+func (qc *QiniuClient) GetToken() string {
+	return qc.generateToken()
 }
